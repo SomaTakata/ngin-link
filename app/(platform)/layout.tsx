bc077b6fc6367bb15a36bcc7ae92cab10e9486f8
Shogo Kitada <contact@shogo0x2e.com>
2024-02-23 13:51:29 +0900

    clerk インストール & 実装

---
+++ app/(platform)/layout.tsx
@@ -0,0 +1,15 @@
+import { ClerkProvider } from "@clerk/nextjs";
+
+const PlatformLayout = ({
+  children
+}: {
+  children: React.ReactNode;
+}) => {
+  return (
+    <ClerkProvider>
+      {children}
+    </ClerkProvider>
+  );
+};
+
+export default PlatformLayout;
