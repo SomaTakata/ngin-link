dd1ccc32587fa66bae51f4bd92ce9c2ba436d344
Shogo Kitada <contact@shogo0x2e.com>
2024-02-23 21:21:46 +0900

    ミス修正

--- hook/process-env.ts
+++ hook/process-env.ts
@@ -2,8 +2,8 @@ import { useMemo } from 'react';
 
 // カスタムフックの定義
 export function useBackendUrl() {
-  
-  const backendUrl = useMemo(() => process.env.REACT_APP_BACKEND_URL, []);
+
+  const backendUrl = useMemo(() => process.env.BACKEND_URL, []);
 
   return backendUrl;
 }
