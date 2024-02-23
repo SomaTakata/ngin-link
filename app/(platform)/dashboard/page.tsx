b752a5580e9995aa8d7d8796b9977e6b02bd87f7
Shogo Kitada <contact@shogo0x2e.com>
2024-02-23 18:11:29 +0900

    Dashboard 名前変更

--- app/(platform)/dashboard/page.tsx
+++ app/(platform)/dashboard/page.tsx
@@ -1,4 +1,4 @@
-const Page = () => {
+const Dashboard = () => {
   return (
     <div>
       <h1>Page Root</h1>
@@ -6,4 +6,4 @@ const Page = () => {
   );
 }
 
-export default Page;
+export default Dashboard;
