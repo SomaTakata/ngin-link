c36023c34acd0f54af4f036c83db4fadb76ec0cb
Shogo Kitada <contact@shogo0x2e.com>
2024-02-24 19:31:29 +0900

    プレースホルダー追加

--- app/(platform)/dashboard/_components/link-form.tsx
+++ app/(platform)/dashboard/_components/link-form.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import { Input } from "@/components/ui/input";
 import { XIcon } from "lucide-react";
 import { SiZenn, SiQiita } from "react-icons/si";
 import { FaGithub } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
-import { url } from "inspector";
 import { Switch } from "@/components/ui/switch";
 
 // propsの型定義
@@ -14,27 +15,33 @@ interface LinkFormProps {
 }
 
 function LinkForm({ platform, urlTo }: LinkFormProps) {
+  const [url, setUrl] = useState(urlTo);
+
   let IconComponent;
   let iconColor = "";
-  let url = "";
+  let placeHolderUrl = "";
 
   // platformの値に応じてアイコンと色を設定
   switch (platform) {
     case "zenn":
       IconComponent = SiZenn;
       iconColor = "text-blue-500";
+      placeHolderUrl = "https://zenn.dev/";
       break;
     case "qiita":
       IconComponent = SiQiita;
       iconColor = "text-green-500";
+      placeHolderUrl = "https://qiita.com/";
       break;
     case "twitter":
       IconComponent = FaXTwitter;
       iconColor = "text-black";
+      placeHolderUrl = "https://twitter.com/";
       break;
     case "github":
       IconComponent = FaGithub;
       iconColor = "text-black";
+      placeHolderUrl = "https://github.com/";
       break;
     default:
       IconComponent = XIcon;
@@ -44,7 +51,12 @@ function LinkForm({ platform, urlTo }: LinkFormProps) {
   return (
     <div className="flex items-center justify-between p-3 bg-gray-100 rounded">
       <IconComponent className={iconColor} size={22} />
-      <Input className="flex-grow mx-2" placeholder={urlTo} />
+      <Input
+        className="flex-grow mx-2"
+        placeholder={placeHolderUrl}
+        value={url}
+        onChange={(e) => setUrl(e.target.value)}
+      />
     </div>
   );
 }
