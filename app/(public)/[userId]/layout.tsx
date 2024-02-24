8d1d3f35dd527c3b7d1f021fdaeff4ff412d4305
Soma Takata <hayatetaka123@gmail.com>
2024-02-25 03:56:57 +0900

    背景を統一

--- app/(public)/[userId]/layout.tsx
+++ app/(public)/[userId]/layout.tsx
@@ -1,7 +1,13 @@
 import { ClerkProvider } from "@clerk/nextjs";
 
 const PlatformLayout = ({ children }: { children: React.ReactNode }) => {
-  return <ClerkProvider>{children}</ClerkProvider>;
+  return (
+    <ClerkProvider>
+      <div className="flex flex-col items-center relative justify-center min-h-screen bg-[#1a1e2e] p-8">
+        {children}
+      </div>
+    </ClerkProvider>
+  );
 };
 
 export default PlatformLayout;
