ef49fc2150f708a094e28d540a8ac1e1666b50a5
Soma Takata <hayatetaka123@gmail.com>
2024-02-24 18:41:04 +0900

    色の調整

--- app/(public)/[userId]/_components/qr-modal.tsx
+++ app/(public)/[userId]/_components/qr-modal.tsx
@@ -9,9 +9,12 @@ const QrModal = ({ params }: { params: { userId: string } }) => {
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <BsQrCodeScan className="text-white absolute top-4 right-4" size={30} />
+        <BsQrCodeScan
+          className="text-white absolute md:top-6 md:right-6 top-4 right-4"
+          size={30}
+        />
       </DialogTrigger>
-      <DialogContent className="w-fit p-10">
+      <DialogContent className="w-fit p-10 rounded-2xl">
         <QRCode className="" value={`${frontUrl}/${params.userId}`} />
       </DialogContent>
     </Dialog>
--- app/(public)/[userId]/page.tsx
+++ app/(public)/[userId]/page.tsx
@@ -24,7 +24,7 @@ const PublicUserPage = ({ params }: { params: { userId: string } }) => {
   const { isSignedIn } = useAuth();
 
   return (
-    <div className="flex flex-col items-center relative justify-center min-h-screen bg-[#1a1e2e] p-4">
+    <div className="flex flex-col items-center relative justify-center min-h-screen bg-[#1a1e2e] p-8">
       {isSignedIn ? <QrModal params={params} /> : <></>}
       <div className="md:w-[732px] ">
         <div className="flex items-center gap-5 mb-6">
@@ -51,46 +51,46 @@ const PublicUserPage = ({ params }: { params: { userId: string } }) => {
             主な職種 : フロントエンドエンジニア
           </p>
         </div>
-        <div className=" gap-6 flex flex-row flex-wrap">
+        <div className="grid md:gap-6 gap-6 md:grid-cols-4 grid-cols-2 ">
           <Link
             href="https://twitter.com/catnose99"
-            className="relative flex items-center justify-center rounded-2xl w-[163.75px] h-[163.75px] aspect-square  bg-c-github shadow-[20px 20px 60px #d5d5d5, -20px -20px 60px #ffffff]"
+            className="relative flex items-center justify-center rounded-2xl md:w-[163.75px] md:h-[163.75px] w-[150px] h-[150px] aspect-square  bg-c-github "
           >
             <div className="flex items-center justify-center">
-              <BsGithub className="h-16 w-16" />
+              <BsGithub className="h-14 w-14" />
             </div>
           </Link>
           <Link
             href="https://twitter.com/catnose99"
-            className="relative flex items-center justify-center rounded-2xl w-[163.75px] h-[163.75px] aspect-square shadow-[20px 20px 60px #d9d9d9, -20px -20px 60px #ffffff] bg-c-x"
+            className="relative flex items-center justify-center rounded-2xl md:w-[163.75px] md:h-[163.75px] w-[150px] h-[150px] aspect-square bg-c-x"
           >
             <div className="flex items-center justify-center">
-              <FaXTwitter className="h-14 w-14" />
+              <FaXTwitter className="h-12 w-12 text-white" />
             </div>
           </Link>
           <Link
             href="https://zenn.dev/catnose99"
-            className="relative flex items-center justify-center rounded-2xl w-[163.75px] h-[163.75px] aspect-square 
-             bg-c-zenn shadow-[20px 20px 60px #bac4d9, -20px -20px 60px #fcffff]"
+            className="relative flex items-center justify-center rounded-2xl md:w-[163.75px] md:h-[163.75px] w-[150px] h-[150px] aspect-square 
+             bg-c-zenn "
           >
             <div className="flex items-center justify-center">
-              <SiZenn className="h-14 w-14" />
+              <SiZenn className="h-12 w-12 text-blue-400" />
             </div>
           </Link>
           <Link
             href="https://twitter.com/catnose99"
-            className="relative flex items-center justify-center rounded-2xl w-[163.75px] h-[163.75px] aspect-square shadow-[20px 20px 60px #c3d9c9, -20px -20px 60px #ffffff] bg-c-qiita"
+            className="relative flex items-center justify-center rounded-2xl md:w-[163.75px] md:h-[163.75px] w-[150px] h-[150px] aspect-square  bg-c-qiita"
           >
             <div className="flex items-center justify-center">
-              <SiQiita className="h-20 w-20" />
+              <SiQiita className="h-20 w-20 text-white" />
             </div>
           </Link>
           <Link
             href="https://twitter.com/catnose99"
-            className="relative flex items-center justify-center rounded-2xl w-[163.75px] h-[163.75px] aspect-square shadow-[20px 20px 60px #c3d9c9, -20px -20px 60px #ffffff] bg-c-person"
+            className="relative flex items-center justify-center rounded-2xl md:w-[163.75px] md:h-[163.75px] w-[150px] h-[150px] aspect-square bg-c-person"
           >
             <div className="flex items-center justify-center">
-              <IoPersonCircleSharp className="h-20 w-20" />
+              <IoPersonCircleSharp className="h-20 w-20 text-white" />
             </div>
           </Link>
         </div>
